5c5ed7d76ce463cad68a67136ef6924765a770f1
Uljaco <ulyssescollins95@gmail.com>
2016-11-23 17:07:04 -0600

    Added camera control using arrow keys. Still need to implement zoom.

--- 3DSnek/_3DSnek/InputManager.cs
+++ 3DSnek/_3DSnek/InputManager.cs
@@ -32,19 +32,29 @@ namespace _3DSnek
         {
             //MouseState mouseState = Mouse.GetState();//or use arrow keys
             Keys[] keys = Keyboard.GetState().GetPressedKeys();
-            float yaw = vom.yaw, pitch = vom.pitch;
+            
             if (keys.Contains<Keys>(Keys.Left))
             {
-                yaw += .05f;
+                vom.yaw += .05f;
             }
             else if (keys.Contains<Keys>(Keys.Right))
             {
-
+                vom.yaw -= .05f;
             }
-
-            Vector3 position = Vector3.Transform(Vector3.Backward, Matrix.CreateFromYawPitchRoll(yaw, pitch, 0f));
+            if (keys.Contains<Keys>(Keys.Up))
+            {
+                vom.pitch += .03f;
+            }
+            else if (keys.Contains<Keys>(Keys.Down))
+            {
+                vom.pitch -= .03f;
+            }
+            //Refactor this stuff over to the VisualOutputManager class
+            Vector3 position = Vector3.Transform(Vector3.Backward, Matrix.CreateFromYawPitchRoll(vom.yaw, vom.pitch, 0f));
+            position *= 6000f;
             position += vom.cameraLookAt;
-            //vom.cameraPosition = position;
+
+            vom.cameraPosition = position;
         }
     }
 }
